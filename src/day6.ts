201aaea650d6fc57f05d247483df2170a705d669
Sam Svindland <sam@Sams-MacBook-Pro.local>
2022-12-06 13:07:00 -0500

    finish day 6

---
+++ src/day6.ts
@@ -0,0 +1,36 @@
+import {readFile} from "fs";
+
+readFile('./src/day6input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    const dataStream = [];
+    data.split("").forEach(value => {
+        dataStream.push(value)
+    });
+
+    const checkIfAllDifferent = (array: string[]) => {
+        const unique = Array.from(new Set(array));
+        return unique.length === array.length;
+    }
+
+    // Part 1
+    let foundStartOfPacked = false;
+    dataStream.forEach((value, index) => {
+        if (!foundStartOfPacked && checkIfAllDifferent(dataStream.slice(index, index + 4))) {
+            console.log(index + 4, dataStream.slice(index, index + 4))
+            foundStartOfPacked = true
+        };
+    });
+
+    // Part 2
+    let foundStartOfMessage = false;
+    dataStream.forEach((value, index) => {
+        if (!foundStartOfMessage && checkIfAllDifferent(dataStream.slice(index, index + 14))) {
+            console.log(index + 14, dataStream.slice(index, index + 14))
+            foundStartOfMessage = true
+        };
+    });
+});
